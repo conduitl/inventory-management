7edeaca531333ca4373e5ee0f13bcafd469dde62
Thomas <thomas.rundle@conduitlearning.com>
2016-07-31 12:47:52 -0500

    Completed part 1 of angular tutorial with my own twist

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,23 @@
 import { Component } from '@angular/core';
+
+export class Plant { 
+    id: number;
+    name: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: '<h1>KJM Inventory Tracker</h1>'
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{plant.name}} details</h2>
+    <div>id: {{plant.id}}</div>
+    <div>name: {{plant.name}}</div>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+    title = 'Plant Catalog';
+    plant: Plant = {
+        id: 1,
+        name: 'cyclamen'
+    };
+ }
