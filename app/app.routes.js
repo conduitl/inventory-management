3607975ed84660f544ee9f7c29fb8981bc8006e6
Thomas <thomas.rundle@conduitlearning.com>
2016-08-03 21:11:35 -0500

    Add gallery view to router

--- app/app.routes.js
+++ app/app.routes.js
@@ -1,10 +1,20 @@
 "use strict";
 var router_1 = require('@angular/router');
 var category_view_component_1 = require('./category-view.component');
+var gallery_view_component_1 = require('./gallery-view.component');
 var routes = [
     {
         path: 'categories',
         component: category_view_component_1.CategoryViewComponent
+    },
+    {
+        path: 'gallery',
+        component: gallery_view_component_1.GalleryViewComponent
+    },
+    {
+        path: '',
+        redirectTo: '/gallery',
+        pathMatch: 'full'
     }
 ];
 exports.appRouterProviders = [
--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,10 +1,20 @@
 import { provideRouter, RouterConfig } from '@angular/router';
 import { CategoryViewComponent } from './category-view.component';
+import { GalleryViewComponent } from './gallery-view.component';
 
 const routes: RouterConfig = [
     {
         path: 'categories',
         component: CategoryViewComponent
+    },
+    {
+        path: 'gallery',
+        component: GalleryViewComponent
+    },
+    {
+        path: '',
+        redirectTo: '/gallery',
+        pathMatch: 'full'
     }
 ];
 
