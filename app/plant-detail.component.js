33e9b302d73c23d57f05a6757fb9cc161260897a
Thomas <thomas.rundle@conduitlearning.com>
2016-08-06 19:40:32 -0500

    Adjustments to layout

--- app/plant-detail.component.js
+++ app/plant-detail.component.js
@@ -15,13 +15,16 @@ var PlantDetailComponent = (function () {
     function PlantDetailComponent(plantService, route) {
         this.plantService = plantService;
         this.route = route;
+        this.lorem = getLorem();
     }
     PlantDetailComponent.prototype.ngOnInit = function () {
         var _this = this;
         this.sub = this.route.params.subscribe(function (params) {
             var id = +params['id'];
             _this.plantService.getPlant(id)
-                .then(function (plant) { return _this.plant = plant; });
+                .then(function (plant) {
+                _this.plant = plant;
+            });
         });
     };
     PlantDetailComponent.prototype.ngOnDestroy = function () {
@@ -33,11 +36,14 @@ var PlantDetailComponent = (function () {
     PlantDetailComponent = __decorate([
         core_1.Component({
             selector: 'plant-detail',
-            template: "\n        <div *ngIf=\"plant\">\n            <h2>{{plant.name}} details</h2>\n            <p>{{plant.description}}</p>\n            <img class=\"img-responsive center-block\" src=\"{{plant.image | slice:0:-4 }}-lg.jpg\"/>\n            <div>\n                <button (click)=\"goBack()\">Back</button>\n            </div>\n        </div>\n    "
+            template: "\n        <div *ngIf=\"plant\">\n            <h2>{{plant.name}} details</h2>\n            <div class=\"row\">\n                <div class=\"col-md-9\">\n                    <p>{{plant.description}}</p>\n                </div>\n            </div>\n            <div class=\"row\">\n                <div class=\"col-md-9\">\n                    <img class=\"img-responsive center-block\" src=\"{{plant.image | slice:0:-4 }}-lg.jpg\"/>\n                </div>\n            </div>\n            <div class=\"row\">\n                <div class=\"col-md-9\">\n                    <p>{{lorem}}</p>\n                </div>\n            </div>\n            <div>\n                <button (click)=\"goBack()\">Back</button>\n            </div>\n        </div>\n    "
         }), 
         __metadata('design:paramtypes', [plant_service_1.PlantService, router_1.ActivatedRoute])
     ], PlantDetailComponent);
     return PlantDetailComponent;
 }());
 exports.PlantDetailComponent = PlantDetailComponent;
+function getLorem() {
+    return "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit risus sem. Donec accumsan vitae eros at pulvinar. Etiam auctor turpis felis, ac pharetra nisi sodales vitae. Fusce ex odio, commodo id metus eget, malesuada efficitur arcu. Etiam commodo vehicula cursus. Donec ultrices neque sed augue eleifend, id luctus erat semper. Vestibulum tellus metus, cursus nec posuere quis, imperdiet dapibus diam. Quisque ut risus ac magna ullamcorper facilisis vitae eu diam. Ut congue tincidunt risus efficitur aliquam. Duis viverra est elit, nec fermentum turpis scelerisque eget. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam a diam lorem. Aliquam molestie laoreet aliquet. Curabitur lectus nisi, fringilla condimentum nunc consectetur, rhoncus porttitor dui. Fusce pulvinar suscipit dolor, non dignissim mauris.\n\nSuspendisse mauris ligula, lacinia a tortor non, accumsan pellentesque turpis. Donec a pulvinar nunc. Nullam ut nunc eget tortor interdum pulvinar. Curabitur in luctus diam. Sed dignissim euismod pellentesque. Sed et libero massa. Nullam ornare commodo risus porta dapibus. Nulla sit amet dolor eu urna fermentum condimentum id sed magna. Sed iaculis, justo at consectetur lobortis, augue lorem suscipit enim, et accumsan nisl risus nec lacus.\n";
+}
 //# sourceMappingURL=plant-detail.component.js.map
--- app/plant-detail.component.ts
+++ app/plant-detail.component.ts
@@ -10,8 +10,21 @@ import { Plant } from './plant';
     template: `
         <div *ngIf="plant">
             <h2>{{plant.name}} details</h2>
-            <p>{{plant.description}}</p>
-            <img class="img-responsive center-block" src="{{plant.image | slice:0:-4 }}-lg.jpg"/>
+            <div class="row">
+                <div class="col-md-9">
+                    <p>{{plant.description}}</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-9">
+                    <img class="img-responsive center-block" src="{{plant.image | slice:0:-4 }}-lg.jpg"/>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-9">
+                    <p>{{lorem}}</p>
+                </div>
+            </div>
             <div>
                 <button (click)="goBack()">Back</button>
             </div>
@@ -24,12 +37,14 @@ export class PlantDetailComponent implements OnInit, OnDestroy {
         private route: ActivatedRoute) {}
     sub: any;
     plant: Plant;
-
+    lorem: string = getLorem();
     ngOnInit() {
         this.sub = this.route.params.subscribe(params => {
             let id = +params['id'];
             this.plantService.getPlant(id)
-                .then(plant => this.plant = plant);
+                .then(plant => {
+                    this.plant = plant
+                });
         });
     }
     ngOnDestroy() {
@@ -39,4 +54,10 @@ export class PlantDetailComponent implements OnInit, OnDestroy {
     goBack() {
         window.history.back();
     }
+}
+function getLorem(){
+    return `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit risus sem. Donec accumsan vitae eros at pulvinar. Etiam auctor turpis felis, ac pharetra nisi sodales vitae. Fusce ex odio, commodo id metus eget, malesuada efficitur arcu. Etiam commodo vehicula cursus. Donec ultrices neque sed augue eleifend, id luctus erat semper. Vestibulum tellus metus, cursus nec posuere quis, imperdiet dapibus diam. Quisque ut risus ac magna ullamcorper facilisis vitae eu diam. Ut congue tincidunt risus efficitur aliquam. Duis viverra est elit, nec fermentum turpis scelerisque eget. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam a diam lorem. Aliquam molestie laoreet aliquet. Curabitur lectus nisi, fringilla condimentum nunc consectetur, rhoncus porttitor dui. Fusce pulvinar suscipit dolor, non dignissim mauris.
+
+Suspendisse mauris ligula, lacinia a tortor non, accumsan pellentesque turpis. Donec a pulvinar nunc. Nullam ut nunc eget tortor interdum pulvinar. Curabitur in luctus diam. Sed dignissim euismod pellentesque. Sed et libero massa. Nullam ornare commodo risus porta dapibus. Nulla sit amet dolor eu urna fermentum condimentum id sed magna. Sed iaculis, justo at consectetur lobortis, augue lorem suscipit enim, et accumsan nisl risus nec lacus.
+`;
 }
