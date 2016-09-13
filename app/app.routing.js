a7321b5d25ad4ff43ca1c059e231fbb80c64c9be
Thomas <thomas.rundle@conduitlearning.com>
2016-09-13 18:35:08 -0500

    Compiled ts file changes, nothing works yet

---
+++ app/app.routing.js
@@ -0,0 +1,40 @@
+"use strict";
+var router_1 = require('@angular/router');
+var category_view_component_1 = require('./components/category-view.component');
+var gallery_view_component_1 = require('./components/gallery-view.component');
+var designs_view_component_1 = require('./components/designs-view.component');
+var supplier_view_component_1 = require('./components/supplier-view.component');
+var plant_detail_component_1 = require('./components/plant-detail.component');
+var appRoutes = [
+    {
+        path: 'categories/:layout',
+        component: category_view_component_1.CategoryViewComponent
+    },
+    {
+        path: 'categories',
+        redirectTo: '/categories/list'
+    },
+    {
+        path: 'gallery',
+        component: gallery_view_component_1.GalleryViewComponent
+    },
+    {
+        path: 'designs',
+        component: designs_view_component_1.DesignsViewComponent
+    },
+    {
+        path: 'suppliers',
+        component: supplier_view_component_1.SupplierViewComponent
+    },
+    {
+        path: '',
+        redirectTo: '/gallery',
+        pathMatch: 'full'
+    },
+    {
+        path: 'detail/:id',
+        component: plant_detail_component_1.PlantDetailComponent
+    }
+];
+exports.routing = router_1.RouterModule.forRoot(appRoutes);
+//# sourceMappingURL=app.routing.js.map
