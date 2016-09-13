9a21c48b89402430bf4550bfb0d8dae3c729475a
Thomas <thomas.rundle@conduitlearning.com>
2016-09-13 17:53:55 -0500

    Update main.ts for RC7

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,3 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { AppComponent } from './components/app.component';
-import { appRouterProviders } from './app.routes';
-bootstrap(AppComponent, [
-    appRouterProviders
-]);
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+platformBrowserDynamic().bootstrapModule(AppModule);
